9653a9119fd354c978f49944af975115550b3cbf
Sayandip Dutta <sayandip199309@gmail.com>
2022-01-29 15:09:06 +0530

    Add tests for delitem and setitem

--- ds/cdll_test.py
+++ ds/cdll_test.py
@@ -12,6 +12,26 @@ def cdll_data():
     """Return a CDLL object"""
     return CDLList(range(10))
 
+@pytest.fixture
+def cdll_data_set():
+    """Return a CDLL object"""
+    return CDLList(range(10))
+
+@pytest.fixture
+def list_data_set():
+    """Return a CDLL object"""
+    return list(range(10))
+
+@pytest.fixture
+def cdll_data_del():
+    """Return a CDLL object"""
+    return CDLList(range(10))
+
+@pytest.fixture
+def list_data_del():
+    """Return a CDLL object"""
+    return list(range(10))
+
 @pytest.mark.parametrize("value, is_error", [
     (slice(2, 10), False),
     (slice(2, 10, 2), False),
@@ -35,7 +55,53 @@ def test_getitem(list_data, cdll_data, value, is_error):
         else:
             assert list(cdll_data[value]) == list_data[value]
 
-def test_cll(cdll_data):
+@pytest.mark.parametrize("index, value, is_error", [
+    (slice(2, 10), list(range(8)), False),
+    (slice(2, 10, 2), list(range(4)), False),
+    (slice(2, None, 3), list(range(3)), False),
+    (slice(None, None, -1), list(range(10)), False),
+    (slice(-2, -10, -1), list(), ValueError),
+    (slice(-2, -10, 'v'), list(), InvalidIntegerSliceError),
+    (12, 0, IndexError), 
+    (-5, 12, False),
+    (slice(None, None, 1), list(), False),
+])
+def test_setitem(list_data_set, cdll_data_set, index, value, is_error):
+    """Test getitem method of CDLL class"""
+    if is_error:
+        with pytest.raises(is_error):
+            cdll_data_set[index] = value
+
+    else:
+        list_data_set[index] = value
+        cdll_data_set[index] = value
+        assert cdll_data_set.size == len(list_data_set)
+        assert list(cdll_data_set) == list_data_set
+
+@pytest.mark.parametrize("value, is_error", [
+    (slice(2, 4), False),
+    (slice(1, None, 6), False),
+    (slice(2, 10, 3), False),
+    (-5, False),
+    (slice(-2, -10, 'v'), InvalidIntegerSliceError),
+    (slice(None, None, -1), False),
+    (12, IndexError), 
+    
+])
+def test_delitem(list_data_del, cdll_data_del, value, is_error):
+    """Test getitem method of CDLL class"""
+    if is_error:
+        with pytest.raises(is_error):
+            print(list(cdll_data_del), value, is_error)
+            del cdll_data_del[value]
+    else:
+        print(list(cdll_data_del), value, is_error)
+        del cdll_data_del[value]
+        del list_data_del[value]
+
+        assert list(cdll_data_del) == list_data_del
+
+def test_iter(cdll_data):
     """Test cdll method of CDLL class"""
     assert list(cdll_data)== [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     assert cdll_data.size == 10
